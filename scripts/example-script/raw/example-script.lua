38c7e3f71d44f71d6f66bdc78f1ebfe525623b1d
Dunno <pitalpitulabib@gmail.com>
2026-09-25 16:38:45 +0700

    Create/update scripts/example-script/raw/example-script.lua

--- scripts/example-script/raw/example-script.lua
+++ scripts/example-script/raw/example-script.lua
@@ -1,10 +1,1 @@
--- CihuyAkz Studio Lite
--- Example Script: starter code you can edit or replace.
-
-local Players = game:GetService("Players")
-
-local function onPlayerAdded(player)
-    print(("Welcome, %s!"):format(player.Name))
-end
-
-Players.PlayerAdded:Connect(onPlayerAdded)
+-- Error loading contenti
